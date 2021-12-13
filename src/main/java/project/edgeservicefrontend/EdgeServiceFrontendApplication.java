342fe2cca875c06ec618e215718692491bf9dc59
Brent Broeckx <r0738889@student.thomasmore.be>
2021-12-13 23:45:50 +0100

    Initial commit

---
+++ src/main/java/project/edgeservicefrontend/EdgeServiceFrontendApplication.java
@@ -0,0 +1,14 @@
+package project.edgeservicefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiceFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServiceFrontendApplication.class, args);
+        f
+    }
+
+}
---
+++ src/test/java/project/edgeservicefrontend/EdgeServiceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package project.edgeservicefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
