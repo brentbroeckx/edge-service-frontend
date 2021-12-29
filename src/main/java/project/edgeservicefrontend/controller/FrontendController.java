896285eab0bb15273ce57a6f8e15c54656735a24
Brent Broeckx <r0738889@student.thomasmore.be>
2021-12-29 02:52:38 +0100

    basic crud from for cars with error!

--- src/main/java/project/edgeservicefrontend/controller/FrontendController.java
+++ src/main/java/project/edgeservicefrontend/controller/FrontendController.java
@@ -1,17 +1,25 @@
 package project.edgeservicefrontend.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.util.json.JSONParser;
+import org.openqa.selenium.json.Json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import project.edgeservicefrontend.model.CarInfo;
 import project.edgeservicefrontend.model.Inspection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -55,6 +63,65 @@ public class FrontendController {
         model.addAttribute("inspections", inspections.getBody());
 
         return "list_inspections";
+    }
+
+    @GetMapping("/car/add")
+    public String addCarPage(final Model model) {
+        CarInfo car = new CarInfo("", "", "", "", CarInfo.PortierOptie.VIERDEURS);
+
+        model.addAttribute("car", car);
+        return "crud_car";
+    }
+
+    @GetMapping("/car/edit/{licensePlate}")
+    public String editCar(@PathVariable String licensePlate, final Model model) {
+        ResponseEntity<CarInfo> car =
+                restTemplate.exchange("https://" + safetyEdgeBaseUrl + "/cars/license_plate/{licensePlate}",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<CarInfo>() {
+                }, licensePlate);
+
+        model.addAttribute("car", car.getBody());
+        return "crud_car";
+    }
+
+    @PostMapping("/car")
+    public String addCar(@ModelAttribute CarInfo car, final Model model) {
+
+        String carLicense = car.getLicensePlate();
+        System.out.println(carLicense);
+
+        if (car.getLicensePlate() != "" && car.getMerk() != "" && car.getType() != "") {
+
+            ResponseEntity<CarInfo> responseEntityCar =
+                    restTemplate.exchange("http://" + safetyEdgeBaseUrl + "/cars/license_plate/{licensePlate}",
+                            HttpMethod.GET, null, new ParameterizedTypeReference<CarInfo>() {
+                            }, carLicense);
+
+            if (responseEntityCar != null) {
+                CarInfo updateCar = responseEntityCar.getBody();
+                updateCar.setLicensePlate(car.getLicensePlate());
+                updateCar.setMerk(car.getMerk());
+                updateCar.setType(car.getType());
+                updateCar.setEuroNorm(car.getEuroNorm());
+                updateCar.setPortier(car.getPortier());
+
+                restTemplate.exchange("http://" + safetyEdgeBaseUrl + "/cars",
+                        HttpMethod.PUT, new HttpEntity<>(updateCar),CarInfo.class);
+            } else {
+                CarInfo newCar = car;
+                CarInfo carInfo =
+                        restTemplate.postForObject("http://" + safetyEdgeBaseUrl + "/cars",
+                                newCar,CarInfo.class);
+            }
+
+            return "list_cars";
+        } else {
+            model.addAttribute("car", car);
+            model.addAttribute("error", "Licenseplate, merk and type are required");
+            return "crud_car";
+        }
+
+
     }
 
 
