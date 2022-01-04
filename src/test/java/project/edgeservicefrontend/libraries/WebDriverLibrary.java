a98f75667dcc15fd5fa4d443f41e4eca6ff1c91b
Brent Broeckx <r0738889@student.thomasmore.be>
2022-01-04 03:21:54 +0100

    less options

--- src/test/java/project/edgeservicefrontend/libraries/WebDriverLibrary.java
+++ src/test/java/project/edgeservicefrontend/libraries/WebDriverLibrary.java
@@ -24,13 +24,8 @@ public class WebDriverLibrary {
         WebDriver driver = null;
         WebDriverManager.chromedriver().browserVersion("96.0.4664.45").setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("enable-automation");
         options.addArguments("--no-sandbox");
-        options.addArguments("--disable-infobars");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-browser-side-navigation");
-        options.addArguments("--disable-gpu");
         driver = new ChromeDriver(options);
 
         return driver;
