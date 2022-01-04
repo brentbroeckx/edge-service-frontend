ce55ca9259bd1f64fd5a02d3906795077588160f
Brent Broeckx <r0738889@student.thomasmore.be>
2022-01-04 03:12:49 +0100

    reverting yaml

--- src/test/java/project/edgeservicefrontend/libraries/WebDriverLibrary.java
+++ src/test/java/project/edgeservicefrontend/libraries/WebDriverLibrary.java
@@ -22,12 +22,11 @@ public class WebDriverLibrary {
 //        return new ChromeDriver();
 
         WebDriver driver = null;
-        WebDriverManager.chromedriver().browserVersion("77.0.3865.40").setup();
+        WebDriverManager.chromedriver().browserVersion("96.0.4664.45").setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         options.addArguments("enable-automation");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
         options.addArguments("--disable-infobars");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-browser-side-navigation");
